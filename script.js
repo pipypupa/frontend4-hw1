6501fdd83e09488c9779a0c2d777565de2a7ae60
pipypupa <zong8729@gmail.com>
2025-03-15 19:43:48 +0200

    hw-1

--- script.js
+++ script.js
@@ -1,56 +1,121 @@
-const galleryContainer = document.querySelector('.gallery');
+const galleryItems = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
 
-if (galleryContainer) {
-  const galleryMarkup = images.map(({ preview, original, description }) => `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
-      </a>
-    </li>
-  `).join('');
+const galleryContainer = document.querySelector(".js-gallery");
+const lightbox = document.querySelector(".js-lightbox");
+const lightboxImage = document.querySelector(".lightbox__image");
+const closeButton = document.querySelector('[data-action="close-lightbox"]');
+const lightboxOverlay = document.querySelector(".lightbox__overlay");
 
-  galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `;
+  })
+  .join("");
+
+galleryContainer.innerHTML = galleryMarkup;
+
+galleryContainer.addEventListener("click", galleryItemClick);
+
+function galleryItemClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  openModal(event.target.dataset.source, event.target.alt);
 }
 
-// Додаємо HTML розмітку модального вікна
-const modalMarkup = `
-  <div class="modal">
-    <div class="modal__content">
-      <span class="modal__close">&times;</span>
-      <img class="modal__image" src="" alt="" />
-    </div>
-  </div>
-`;
-document.body.insertAdjacentHTML('beforeend', modalMarkup);
+function openModal(src, alt) {
+  lightbox.classList.add("is-open");
+  lightboxImage.src = src;
+  lightboxImage.alt = alt;
+  window.addEventListener("keydown", escKeyPress);
+}
 
-// Отримуємо елементи модального вікна
-const modal = document.querySelector('.modal');
-const modalImg = document.querySelector('.modal__image');
-const modalClose = document.querySelector('.modal__close');
+function closeModal() {
+  lightbox.classList.remove("is-open");
+  lightboxImage.src = "";
+  lightboxImage.alt = "";
+  window.removeEventListener("keydown", escKeyPress);
+}
 
-// Відкриття модального вікна при кліку на картинку
-galleryContainer?.addEventListener('click', (event) => {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') return;
-  
-  modal.classList.add('open');
-  modalImg.src = event.target.dataset.source;
-  modalImg.alt = event.target.alt;
-});
+closeButton.addEventListener("click", closeModal);
 
-// Закриття модального вікна при натисканні на кнопку
-modalClose.addEventListener('click', closeModal);
+lightboxOverlay.addEventListener("click", closeModal);
 
-// Закриття модального вікна при натисканні на клавішу Escape
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') {
+function escKeyPress(event) {
+  if (event.code === "Escape") {
     closeModal();
   }
-});
-
-// Функція для закриття модального вікна та очищення src
-function closeModal() {
-  modal.classList.remove('open');
-  modalImg.src = '';
-  modalImg.alt = '';
 }
